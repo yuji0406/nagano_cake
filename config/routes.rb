897a0af04d4bf6c2d456a147f3f889fc66bbd4bd
yuji0406 <ky19930305@gmail.com>
2021-07-05 13:55:33 +0000

    [Add]管理者側のルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
   devise_for :customers
 
   scope module: :public do
+    root to:'homes#top'
+    get 'homes/about' => 'homes#about'
     resource :customers,only:[:show,:edit,:update]
       get 'customers/check' => 'customers#check'
       patch 'customers/withdraw' => 'customers#withdraw'
     resources :items,only:[:index,:show]
-    resources :sessions,only:[:new,:create,:destroy]
-    resources :customers,only:[:show,:edit,:update,]
-      patch 'customers/withdraw' => 'cusomers#withdraw'
-      get 'cusomers/check' => 'cusomers#check'
     resources :cart_items,except:[:new,:show,:edit]
       delete 'cart_items/empty' => 'cart_items#empty'
     resources :orders,only:[:new,:create,:index,:show]
@@ -17,5 +15,15 @@ Rails.application.routes.draw do
       get 'orders/complete' => 'orders#complete'
     resources :addresses,except:[:show,:new]
   end
-
+  namespace :admin do
+    get '/sign_in' => 'session#new'
+    post '/sign_in' => 'session#create'
+    delete '/sign_out' => 'session#destroy'
+    get '/' => "homes#top"
+    resources :items,except:[:destroy]
+    resources :genres,only:[:index,:create,:edit,:update]
+    resources :customers,only:[:index,:show,:edit,:update]
+    resources :orders,only:[:show,:update]
+    resources :order_items,only:[:update]
+  end
 end
