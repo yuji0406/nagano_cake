0ad5bf055d6482aa8f2eb49b4cd1fc50b86a0512
yuji0406 <ky19930305@gmail.com>
2021-07-07 10:21:08 +0000

    [Add]admin/homesコントローラのアクション追加

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,11 +1,18 @@
 class Admin::SessionsController < ApplicationController
   def new
+    @customer=Customer.new
   end
   
   def create
+    @customer=Customer.new(customer_params)
+    @customer.save
   end
   
   def destroy
   end
   
+  private
+  def customer_params
+    params.require(:customer).permit(:email,:password)
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  
+  @full_name=customer.first_name+customer.last_name
+  
 end
