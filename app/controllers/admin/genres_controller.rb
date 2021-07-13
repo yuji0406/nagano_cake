b5ad662610fe5f1185117fc008dfee45ac87cfd4
yuji0406 <ky19930305@gmail.com>
2021-07-13 15:18:46 +0000

    [Add]管理者用のジャンルの一覧ページと新規登録機能を作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,15 +1,26 @@
 class Admin::GenresController < ApplicationController
   layout 'admin'
   def index
+    @genres=Genre.all
+    @genre=Genre.new
   end
 
   def edit
   end
 
   def create
+    @genre=Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def update
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 
 end
