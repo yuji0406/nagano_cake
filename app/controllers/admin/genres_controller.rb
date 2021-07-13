7eb1c9691169610c9517258bb83f9af267abd96b
yuji0406 <ky19930305@gmail.com>
2021-07-13 15:33:58 +0000

    [Add]ジャンル編集ページと変更機能の作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,6 +6,7 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre=Genre.find(params[:id])
   end
 
   def create
@@ -15,10 +16,13 @@ class Admin::GenresController < ApplicationController
   end
 
   def update
+    @genre=Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
-  
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
