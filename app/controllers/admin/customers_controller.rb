e07fa96ebd70ed4c1f0a96d945c62da54fd9bc24
yuji0406 <ky19930305@gmail.com>
2021-07-14 08:46:45 +0000

    [Update]会員詳細ページの更新

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,16 +2,17 @@ class Admin::CustomersController < ApplicationController
   layout 'admin'
   def index
      @customers=Customer.page(params[:page]).per(10)
-     
+
   end
 
   def show
+    @customer=Customer.find(params[:id])
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
 end
