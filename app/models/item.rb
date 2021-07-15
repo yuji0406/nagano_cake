c503520c3f23804454b091e4af5efe89e9d365df
yuji0406 <ky19930305@gmail.com>
2021-07-15 23:16:09 +0000

    [ Add]管理者用の注文詳細画面の作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_items
   belongs_to :genre
+  
 end
--- db/migrate/20210705082637_devise_create_customers.rb
+++ db/migrate/20210705082637_devise_create_customers.rb
@@ -38,7 +38,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       t.string :postal_code
       t.string :address
       t.string :telephone_number
-      t.boolean :is_active
+      t.boolean :is_active,default: true
 
 
       t.timestamps null: false
