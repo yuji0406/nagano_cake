36baef7f5ae3f2deb671283404560c72f6c6ca29
yuji0406 <ky19930305@gmail.com>
2021-07-18 00:34:45 +0000

    [Add]会員用のトップ、アバウト、商品一覧画面の作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,11 @@
 class Public::ItemsController < ApplicationController
   layout 'public'
   def index
+    @genres=Genre.all
+    @items=Item.page(params[:page]).per(8)
+  end
+  
+  def search
   end
 
   def show
